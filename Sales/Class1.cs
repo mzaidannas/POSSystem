5dc701f9d78ea0cb497de0e7f6636b26df82a4f4
Zaid Annas <bcsf12a527@pucit.edu.pk>
2017-03-31 19:38:31 +0500

    App structure created witout web api.

---
+++ Sales/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem.BLL.Sales
+{
+    public class Class1
+    {
+    }
+}
