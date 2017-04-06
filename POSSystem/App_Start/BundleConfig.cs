1db6679b9dee56c43cc2f3b2ac087a52b1f55da5
Zaid Annas <bcsf12a527@pucit.edu.pk>
2017-04-06 23:45:27 +0500

    bootstrap-sass added for advanced styling

--- POSSystem/App_Start/BundleConfig.cs
+++ POSSystem/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace POSSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/lib/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/lib/jquery-validate/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/lib/bootstrap/bootstrap.js",
+                      "~/Scripts/lib/respond/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      //"~/Content/lib/bootstrap/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
---
+++ POSSystem/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POSSystem.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+        }
+
+        [Authorize]
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
